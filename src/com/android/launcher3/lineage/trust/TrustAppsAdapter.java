23bb3dce9a8c7caa38d789ddd16d264047a16892
Edwiin Kusuma Jaya <kutemeikito0905@gmail.com>
2023-06-22 14:02:24 +0000

    Launcher3: Rebrand to ClownUI
    Signed-off-by: Edwiin Kusuma Jaya <kutemeikito0905@gmail.com>

--- src/com/android/launcher3/lineage/trust/TrustAppsAdapter.java
+++ src/com/android/launcher3/lineage/trust/TrustAppsAdapter.java
@@ -33,7 +33,7 @@ import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.DiffUtil;
 import androidx.recyclerview.widget.RecyclerView;
 
-import com.android.internal.util.banana.BananaUtils;
+import com.android.internal.util.clown.ClownUtils;
 import com.android.launcher3.R;
 import com.android.launcher3.lineage.trust.db.TrustComponent;
 
@@ -108,7 +108,7 @@ class TrustAppsAdapter extends RecyclerView.Adapter<TrustAppsAdapter.ViewHolder>
 
             mProtectedView.setVisibility(hasSecureKeyguard ? View.VISIBLE : View.GONE);
 
-            mHiddenView.setVisibility(BananaUtils.launchablePackages(mContext).contains(component.getPackageName()) ?
+            mHiddenView.setVisibility(ClownUtils.launchablePackages(mContext).contains(component.getPackageName()) ?
                     View.VISIBLE : View.GONE);
 
             mHiddenView.setOnClickListener(v -> {
